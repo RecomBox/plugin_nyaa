c61582df7bfd8c0e2286a762c6760cac8c87702d
GoodDay360 <istartgame31@gmail.com>
2026-05-11 18:26:40 +0700

    fix: fix esbuild

--- src/get_sources.ts
+++ src/get_sources.ts
@@ -1,25 +1,11 @@
 import request from "@plugin_provider/method/request";
-import { parseHTML } from 'linkedom';
+import { parse } from 'node-html-parser';
 
 import type { InputPayload, OutputPayload } from "@plugin_provider/global/types/get_sources";
 
-// Define an internal interface to ensure type safety within the function
-interface TorrentData {
-    id: string;
-    title: string;
-    seeds: number;
-}
-
 export default async function start_get_sources(input_payload: InputPayload): Promise<OutputPayload> {
-    let prefer_query: string = "";
-
-    if (input_payload.search) {
-        prefer_query = input_payload.search;
-    } else {
-        prefer_query = input_payload.title_secondary || "";
-    }
-
-    const url = `https://nyaa.si/?q=${encodeURIComponent(prefer_query)}&f=0&c=0_0&p=${input_payload.page}`;
+    const prefer_query = input_payload.search || input_payload.title_secondary || "";
+    const url = `https://nyaa.si/?q=${prefer_query}&f=0&c=0_0&p=${input_payload.page}`;
 
     const res = new request({
         url,
@@ -27,42 +13,30 @@ export default async function start_get_sources(input_payload: InputPayload): Pr
     });
     await res.send();
     
-    const { document } = parseHTML(res.body_text());
-
-    const table = document.querySelector("table.torrent-list");
-    // Explicitly cast to an array-like or use Array.from if boa supports it.
-    // However, a simple null check is safest for the engine.
-    const tr_list = table?.querySelectorAll("tbody tr");
-
-    let data: TorrentData[] = [];
+    const root = parse(res.body_text());
+    const table = root.querySelector("table.torrent-list")!;
     
-    if (tr_list) {
-        // Using a standard for loop is the "safest" for boa_engine compatibility
-        for (let i = 0; i < tr_list.length; i++) {
-            const tr = tr_list[i];
-            const td_list = tr.querySelectorAll("td");
-            
-            if (td_list.length < 6) continue;
+    const tr_list = table.querySelectorAll("tbody tr");
+    const data: { id: string; title: string; seeds: number }[] = [];
+    
+    for (const i in tr_list) {
+        const tr = tr_list[i];
+        const td_list = tr?.querySelectorAll("td")!;
+        
+        const a_ele = td_list[1]!.querySelector("a:not(.comments)")!;
+        
+        const raw_title = a_ele.getAttribute("title")!.trim();
+        const seeds_str = td_list[5]!.text;
+        const href = a_ele.getAttribute("href")!;
+        const id = href.split("/").pop()!;
 
-            const a_ele = td_list[1].querySelector("a:not(.comments)");
-            
-            const raw_title: string = a_ele?.getAttribute("title")?.trim() || "Unknown Title";
-            const seeds_text: string = td_list[5].textContent || "0";
-
-            const href: string = a_ele?.getAttribute("href") || "";
-            const id: string = href.split("/").pop() || "";
-
-            const title: string = `${raw_title} [seeders: ${seeds_text}]`;
-
-            data.push({
-                id: id,
-                title: title,
-                seeds: parseInt(seeds_text, 10) || 0
-            });
-        }
+        data.push({
+            id,
+            title: `${raw_title} [seeders: ${seeds_str}]`,
+            seeds: parseInt(seeds_str, 10) || 0
+        });
     }
 
     data.sort((a, b) => b.seeds - a.seeds);
-
     return data;
 }
--- src/get_torrents.ts
+++ src/get_torrents.ts
@@ -1,37 +1,32 @@
-// import request from "@plugin_provider/method/request";
-// import * as cheerio from 'cheerio';
-// import { decodeHTML } from 'entities';
+import request from "@plugin_provider/method/request";
+import { parse } from 'node-html-parser';
+import { decodeHTML } from 'entities';
 
-// import type { InputPayload, OutputPayload } from "@plugin_provider/global/types/get_torrents";
+import type { InputPayload, OutputPayload } from "@plugin_provider/global/types/get_torrents";
 
+export default async function start_get_torrents(input_payload: InputPayload): Promise<OutputPayload> {
 
+    let url = `https://nyaa.si/view/${input_payload.id}`;
 
-// export default async function start_get_torrents(input_payload: InputPayload): Promise<OutputPayload> {
+    let res = new request({
+        url,
+        method: "get"
+    });
+    await res.send();
 
-
-//     let url = `https://nyaa.si/view/${input_payload.id}`;
-
-//     let res = new request({
-//         url,
-//         method: "get"
-//     });
-//     await res.send();
-
+    let root = parse(res.body_text());
     
+    let raw_title = decodeHTML(root.querySelectorAll("h3.panel-title")[0]!.text.trim());
 
-//     let cio = cheerio.load(res.body_text());
-//     let raw_title = decodeHTML(cio("h3.panel-title").eq(0).text().trim());
+    let panel_body = root.querySelectorAll("div.panel-body")[0]!;
 
+    let seeders = panel_body.querySelectorAll("div.row")[1]!.querySelector("div.col-md-5 span")!.text.trim();
+
+    let title = `${raw_title} [seeders: ${seeders}]`;
+    let torrent_url = `https://nyaa.si/download/${input_payload.id}.torrent`
     
-//     let panel_body = cio("div.panel-body").eq(0);
-
-//     let seeders = panel_body.find("div.row").eq(1).find("div.col-md-5").find("span").text();
-
-//     let title = `${raw_title} [seeders: ${seeders}]`;
-//     let torrent_url = `https://nyaa.si/download/${input_payload.id}.torrent`
-    
-//     return [{
-//         title,
-//         torrent_url: torrent_url,
-//     }]
-// }
+    return [{
+        title,
+        torrent_url: torrent_url,
+    }]
+}
