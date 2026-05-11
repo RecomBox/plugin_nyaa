07c35afba3e3deac90e3c7f57c396763d660358b
GoodDay360 <istartgame31@gmail.com>
2026-05-11 18:04:23 +0700

    fix: fix esbuild

--- esbuild.config.js
+++ esbuild.config.js
@@ -6,7 +6,7 @@ import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfil
 await build({
     entryPoints: ["src/plugin.ts"],   // main entry, imports like test.ts are included
     bundle: true,                     // follow imports and bundle them
-    platform: "neutral",              // avoid Node built-ins
+    platform: "node",              // avoid Node built-ins
     format: "iife",                   // wrap into one global file
     minify: true,                     // shrink internal helpers
     outfile: "dist/plugin.js",        // output location
